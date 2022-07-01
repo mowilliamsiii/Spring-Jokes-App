634e19f1c80e181a829434d7079a89803d6ed0f9
mowilliamsiii <moseswilliamsiii99@gmail.com>
2022-07-01 17:02:46 -0400

    Created Jokes Service

--- src/main/java/com/moseswilliamsiii/springjokesapp/SpringJokesAppApplication.java
+++ src/main/java/com/moseswilliamsiii/springjokesapp/SpringJokesAppApplication.java
@@ -1,5 +1,7 @@
 package com.moseswilliamsiii.springjokesapp;
 
+import com.moseswilliamsiii.services.JokesService;
+import com.moseswilliamsiii.services.JokesServiceImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +9,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringJokesAppApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(SpringJokesAppApplication.class, args);
+
+        JokesService jokesService = new JokesServiceImpl();
+
+        System.out.println(jokesService.getRandomJoke());
     }
 
 }
